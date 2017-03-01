161417126eb52ffe38d646832b494bd051157d22
Bradley LaVigne <bradley.lavigne@schoolspecialty.com>
2017-03-01 17:44:35 -0600

    Add a function to create a partial map from an SVN URL

--- migrate/new-nuke.sh
+++ migrate/new-nuke.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-readonly blank_migration="# Commented properties are optional
+blank_migration() {
+	echo "# Commented properties are optional
 
 svn-url = ${default[svn-url]}
 # svn-dir = ${default[svn-dir]}
 
 git-url = ${default[git-url]}
-# git-dir = ${default[git-dir]}
 
 authors-file = ${default[authors-file]}
 default-domain = ${default[default-domain]}
@@ -14,35 +14,58 @@ trunk = ${default[trunk]}
 # branches = ${default[branches]}
 # tags = ${default[tags]}
 "
+}
+
+less_blank_migration() {
+	local repo="${1#*svn'/'}"
+
+	echo "# Commented properties are optional
+
+svn-url = https://svn.schoolspecialty.com/svn/${repo%%'/'*}
+svn-dir = ${repo##*'/'}
+
+git-url = 
+
+authors-file = ${default[authors-file]}
+default-domain = ${default[default-domain]}
+
+trunk = ${repo#*'/'}:refs/heads/master
+# branches = ${default[branches]}
+# tags = ${default[tags]}
+"
+}
 
 new_migration() {
-    local name="${1%.migration}.migration"
+	local name="${1%.migration}.migration"
 
 	if [[ -z "$1" ]]; then
 		err "Usage: new <repo-name>"
 		return 2
-    elif [[ -e "${name}" ]]; then
-        err "file already exists: ${name}"
-        return 1
+	elif [[ -e "${name}" ]]; then
+		err "file already exists: ${name}"
+		return 1
+	elif [[ -n "$2" ]]; then
+		echo "$(less_blank_migration "$2")" > "${name}"
 	else
-        echo "${blank_migration}" > "${name}"
-        eval "${core[editor]} ${name}"
-    fi
+		echo "$(blank_migration)" > "${name}"
+	fi
+	
+	eval "${core[editor]} ${name}"
 }
 
 nuke() {
 	local the_site="${project_path}/${migration[git-dir]}"
 
-    if [[ ! -d "${the_site}" ]]; then
-        err "nuke: directory not found: ${the_site}"
-        return 1
-    fi
+	if [[ ! -d "${the_site}" ]]; then
+		err "nuke: directory not found: ${the_site}"
+		return 1
+	fi
 
-    msg "Nuking the the site from orbit. It's the only way to be sure..."
-    info "This will permanently delete ${the_site}"
-    read -p "Are you sure? (yes/no) "
-    if [[ "${REPLY}" = "yes" ]]; then
-        subgit uninstall --purge "${the_site}"
-        rm -rfd "${the_site}"
-    fi
+	msg "Nuking the the site from orbit. It's the only way to be sure..."
+	info "This will permanently delete ${the_site}"
+	read -p "Are you sure? (yes/no) "
+	if [[ "${REPLY}" = "yes" ]]; then
+		subgit uninstall --purge "${the_site}"
+		rm -rfd "${the_site}"
+	fi
 }
